0e7d23b8bf30dc5fddda4c3c268a3fb0dd8e3520
zacdeardorff <zachdear73@gmail.com>
2020-04-28 16:57:43 -0500

    Changed code to print larger sprites

--- software/final-project/graphics.cpp
+++ software/final-project/graphics.cpp
@@ -10,7 +10,15 @@
 // Pointer to registers inside of graphics interface
 volatile unsigned int * GRAPHICS_PTR = (unsigned int *) AVALON_GRAPHICS_INTERFACE_0_BASE;
 
-void drawImg(int img_id, int imgX, int imgY){
+#define SPRITE_SIZE 16
+void drawImg(int img_id, int imgX, int imgY){ //top left top right bottom left bottom right
+		drawSprite(img_id , imgX , imgY);
+		drawSprite(img_id +1, imgX + SPRITE_SIZE, imgY);
+		drawSprite(img_id +2, imgX, imgY + SPRITE_SIZE);
+		drawSprite(img_id +3, imgX + SPRITE_SIZE, imgY + SPRITE_SIZE);
+}
+
+void drawSprite(int img_id, int imgX, int imgY){
 	// Set img_id, imgX, and imgY
 	GRAPHICS_PTR[0] = img_id;
 	GRAPHICS_PTR[1] = imgX;
@@ -26,5 +34,3 @@ void drawImg(int img_id, int imgX, int imgY){
 	// Lower start flag
 	GRAPHICS_PTR[3] = 0;
 }
-
-
